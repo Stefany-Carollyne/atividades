e5f6718bb41a4baf992f730fa8b749be76b5ced5
Stéfany Carollyne Bento da Silva <152645508+Stefany-Carollyne@users.noreply.github.com>
2023-12-02 18:46:57 -0300

    Update 14.c

--- 14.c
+++ 14.c
@@ -1,14 +1,14 @@
  int main() {
   int A, B, C, q1, q2, q3, junto, total;
-  printf("Digite o valor de A");
+  printf("digite o valor de A");
   scanf("%i", &A);
-  printf("Digite o valor de B");
+  printf("digite o valor de B");
   scanf("%i", &B);
-  printf("Digite o valor de C");
+  printf("digite o valor de C");
   scanf("%i", &C);
   q1 = A * A;
   q2 = B * B;
-  q3 = B * B;
+  q3 = C * C;
   printf("o quadrado de A e: %i", q1);
   printf("o quadrado de B e: %i", q2);
   printf("o quadrado de C e: %i", q3);
@@ -16,6 +16,5 @@
   printf("o junto e: %i", junto);
   total = junto * junto;
   printf ("o total e: %i",total);
-
-    return 0;
+  return 0;
 }
