95a81341d65d43709fd58382e79024542e51918a
Stéfany Carollyne Bento da Silva <152645508+Stefany-Carollyne@users.noreply.github.com>
2023-12-02 18:49:12 -0300

    Update 8.c

--- 8.c
+++ 8.c
@@ -4,7 +4,7 @@ int main() {
   scanf("%f", &raio);
   area = 3,14 * raio * raio;
   comprimento = 2 * 3,14 * raio;
-  printf("A area da circunferência e: %f", area);
-  printf("O comprimento da circunferência e: %f", comprimento);
+  printf("A area da circunferencia e: %f", area);
+  printf("O comprimento da circunferencia e: %f", comprimento);
   return 0;
 }
