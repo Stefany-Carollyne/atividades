14cd75696455fe2f32bf590156710918d47e1f04
Stéfany Carollyne Bento da Silva <152645508+Stefany-Carollyne@users.noreply.github.com>
2023-12-02 18:53:43 -0300

    Update 15.c

--- 15.c
+++ 15.c
@@ -2,7 +2,7 @@ int main() {
   float km, litros, consumo;
   printf("digite os km rodados");
   scanf("%f", &km);
-  printf("digite a quantidade de litros de combustível: ");
+  printf("digite a quantidade de litros de combustivel: ");
   scanf("%f", &litros);
   consumo = km / litros;
   printf("O consumo e: %f", consumo);
