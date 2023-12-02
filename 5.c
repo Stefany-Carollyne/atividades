3abbf0202016c40f23582cb2f4d5fffc15dff834
Stéfany Carollyne Bento da Silva <152645508+Stefany-Carollyne@users.noreply.github.com>
2023-12-02 18:52:22 -0300

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,13 +1,13 @@
 float main() {
   float num1, num2, soma, subtracao, multiplicacao,divisao;
-  printf("digite o primeiro número");
+  printf("digite o primeiro numero");
   scanf("%f", &num1);
-  printf("digite o segundo número");
+  printf("digite o segundo numero");
   scanf("%f", &num2);
   soma = num1 + num2;
   subtracao = num1 - num2; 
   multiplicacao = num1 * num2;
-  divisao = num1 / num2
+  divisao = num1 / num2;
   printf("a soma e: %f", soma);
   printf("a subtracao e: %f", subtracao);
   printf("a multiplicacao e: %f", multiplicacao);
