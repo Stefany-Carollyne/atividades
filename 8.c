48106f84f644968e31f5db44cd1003819957d2f3
Stéfany Carollyne Bento da Silva <152645508+Stefany-Carollyne@users.noreply.github.com>
2023-12-02 18:38:28 -0300

    Update 8.c

--- 8.c
+++ 8.c
@@ -1,9 +1,10 @@
 int main() {
   float raio, area, comprimento;
-  printf("digite o raio da circunferencia: ");
+  printf("digite o raio da circunferencia");
   scanf("%f", &raio);
   area = 3,14 * raio * raio;
-  diametro = 2 * 3,14 * raio;
-  printf("A área da circunferência e: %f", area);
-  printf("O comprimento da circunferência é: %f", comprimento);
+  comprimento = 2 * 3,14 * raio;
+  printf("A area da circunferÃªncia e: %f", area);
+  printf("O comprimento da circunferÃªncia e: %f", comprimento);
   return 0;
+}
