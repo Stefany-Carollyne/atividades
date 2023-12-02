c959de725175b5e93191a5aa7d2bb6f37b71f9ad
Stéfany Carollyne Bento da Silva <152645508+Stefany-Carollyne@users.noreply.github.com>
2023-12-02 18:46:04 -0300

    Update 13.c

--- 13.c
+++ 13.c
@@ -1,19 +1,18 @@
  int main() {
   int A, B, C, q1, q2, q3, resultado;
-  printf("Digite o valor de A");
+  printf("digite o valor de A");
   scanf("%i", &A);
-  printf("Digite o valor de B");
+  printf("digite o valor de B");
   scanf("%i", &B);
-  printf("Digite o valor de C");
+  printf("digite o valor de C");
   scanf("%i", &C);
   q1 = A * A;
   q2 = B * B;
-  q3 = B * B;
+  q3 = C * C;
   printf("o quadrado de A e: %i", q1);
   printf("o quadrado de B e: %i", q2);
   printf("o quadrado de C e: %i", q3);
-    resultado = q1 + q2 + q3;
-    printf("o resultado e: %i", resultado);
-
-    return 0;
+  resultado = q1 + q2 + q3;
+  printf("o resultado e: %i", resultado);
+  return 0;
 }
