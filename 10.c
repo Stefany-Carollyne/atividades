350383735437bbed4dbcb2d3f68a0e1c1412a224
Stéfany Carollyne Bento da Silva <152645508+Stefany-Carollyne@users.noreply.github.com>
2023-12-02 18:42:55 -0300

    Update 10.c

--- 10.c
+++ 10.c
@@ -1,11 +1,12 @@
 int main() {
 float base_maior, base_menor, altura, area; 
-printf("Digite o comprimento da base maior do trapézio");
+printf("digite o comprimento da base maior do trapezio");
 scanf("%f", &base_maior);
-printf("Digite o comprimento da base menor do trapézio: "); 
+printf("digite o comprimento da base menor do trapezio: "); 
 scanf("%f", &base_menor);
-printf("Digite a altura do trapézio: ");
+printf("digite a altura do trapezio: ");
 scanf("%f", &altura);
-float area = (base_maior + base_menor) * altura / 2.0;
- printf("A area do trapézio é: %f", area);
+area = (base_maior + base_menor) * altura / 2.0;
+printf("A area do trapezio Ã©: %f", area);
+return 0;
 }
