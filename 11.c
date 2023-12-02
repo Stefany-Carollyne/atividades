277ca996f591cf15c5d6a3c7ead0af951b481741
Stéfany Carollyne Bento da Silva <152645508+Stefany-Carollyne@users.noreply.github.com>
2023-12-02 18:43:46 -0300

    Update 11.c

--- 11.c
+++ 11.c
@@ -1,8 +1,8 @@
 int main() {
   float temperatura_celsius, temperatura_fahrenheit;
-  printf("Digite a temperatura em Celsius: ");
-    scanf("%f", &temperatura_celsius);
-   temperatura_fahrenheit = (temperatura_celsius * 9/5) + 32;
-    printf("A temperatura em Fahrenheit é: %f", temperatura_fahrenheit);
-      return 0; 
+  printf("digite a temperatura em Celsius: ");
+  scanf("%f", &temperatura_celsius);
+  temperatura_fahrenheit = (temperatura_celsius * 9/5) + 32;
+  printf("A temperatura em Fahrenheit e: %f", temperatura_fahrenheit);
+  return 0; 
 }
